02625540364176b600bea2f162c3592483b0a805
ChrisCrossed <chriscrossed.86@gmail.com>
2021-07-18 23:03:28 -0700

    CSV file updates

--- Assets/Scripts/c_ProgramData.cs
+++ Assets/Scripts/c_ProgramData.cs
@@ -1,6 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Windows;
+using System.Text;
+using System.IO;
+using UnityEditor;
+using System.Linq;
+
+public struct UserData
+{
+    private string _userName;
+    private int _score;
+
+    public string UserName
+    {
+        get => _userName;
+        private set => _userName = value;
+    }
+
+    public int Score
+    {
+        get => _score;
+        internal set =>_score = value;
+    }
+    
+    internal UserData(string userName_, int score_)
+    {
+        _userName = userName_;
+        _score = score_;
+    }
+}
 
 public class c_ProgramData : MonoBehaviour
 {
@@ -8,7 +37,7 @@ public class c_ProgramData : MonoBehaviour
     private static c_ProgramData instance = null;
     private void Awake()
     {
-        if(instance == null)
+        if (instance == null)
         {
             instance = this;
 
@@ -17,7 +46,7 @@ public class c_ProgramData : MonoBehaviour
         else
         {
             Destroy(this.gameObject);
-            
+
             return;
         }
 
@@ -33,17 +62,108 @@ public class c_ProgramData : MonoBehaviour
 
     string[] ChatCommands;
 
+    List<UserData> userDataList = new List<UserData>();
+
+    // public void AddUser(string userName_) => userDataList.Add(new UserData(userName_, 50));
+    public void AddUser(string userName_)
+    {
+        if(!DoesUserNameExist(userName_))
+        {
+            userDataList.Add(new UserData(userName_, 100));
+
+            SaveData();
+        }
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
         ChatCommands = new string[5];
 
         print("Start Called");
+
+        // UserData newData = new UserData();
+
+        AddUser("ChrisCrossed");
+        AddUser("Auston");
+        AddUser("Auston");
+        AddUser("1337Hacker");
+        AddUser("CoolGuy42");
+        AddUser("ChrisCrossed");
+        AddUser("TheONE");
+        // AddUser("ImCOOOL");
+
+        userDataList = userDataList.OrderBy(o => o.UserName).ToList();
+    }
+
+    bool FirstSaveAttempt = false;
+    string Folder;
+    string FilePath;
+    void SaveData()
+    {
+        if(FirstSaveAttempt)
+        {
+            // string folder = "%UserProfile%/TarkovRandomizer/";
+            Folder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments).Replace("\\", "/");
+            Folder += "/TarkovRandomizer/";
+
+            if (!System.IO.Directory.Exists(Folder)) System.IO.Directory.CreateDirectory(Folder);
+
+            FilePath = Path.Combine(Folder, "users.csv");
+
+            FirstSaveAttempt = false;
+        }
+
+        WriteData();
+    }
+
+    bool DoesUserNameExist(string _userName)
+    {
+        bool foundName = false;
+
+        foreach(UserData user in userDataList)
+        {
+            if(user.UserName == _userName)
+            {
+                foundName = true;
+                continue;
+            }
+        }
+
+        return foundName;
+    }
+
+    void WriteData()
+    {
+        StreamWriter writer = new StreamWriter(FilePath, false);
+
+        using (writer)
+        {
+            foreach (UserData data in userDataList)
+                writer.Write(data.UserName + "," + data.Score + System.Environment.NewLine);
+        }
+
+        Debug.Log($"CSV File written to \"{FilePath}\"");
+
+        #if UNITY_EDITOR
+        UnityEditor.AssetDatabase.Refresh();
+        #endif
+    }
+
+    void AddData()
+    {
+
+    }
+
+    void ReadData()
+    {
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
